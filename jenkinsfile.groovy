093038ccd42e8a8c5ee1184267afdaf337edb008
cloudperis <cloudperis1.gmail.com>
2021-09-03 17:17:12 -0400

    update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -28,6 +28,17 @@ pipeline {
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
             }
+        stage('Test') {
+            steps {
+               junit '**/target/surefire-reports/TEST-*.xml' 
+            }
+        }
+
+        stage('Deploy') {
+            steps {
+                archiveArtifacts 'target/*.jar'
+            }
+        }
 
             post {
                 // If Maven was able to run the tests, even if some of the test
