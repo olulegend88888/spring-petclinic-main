5ca1483bcb450b7e37814f83fbcb12a8a8889a4c
cloudperis <cloudperis1.gmail.com>
2021-09-03 18:47:18 -0400

    jenkinsfile created

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -10,44 +10,55 @@ pipeline {
 
     stages {
         
-        stage('check distro'){
-           steps {
-               sh "java -version"
-           }
-        }
-        stage('Build') {
+        stage('Source') {
             steps {
                 // Get some code from a GitHub repository
-                 git 'https://github.com/cloudperis/spring-petclinic-main.git'
+                 sh  'git clone https://github.com/cloudperis/spring-petclinic-main.git'
+                 sh 'ls'
+
+            }
+        }
+        stage('Build') {
+            steps{
+                sh "mvn package"
+
+            }
+
+        }
 
                 // Run Maven on a Unix agent.
-                sh "ls"
-                sh "pwd"
-                sh "mvn package"
+                
+                
 
                 // To run Maven on a Windows agent, use
                 // bat "mvn -Dmaven.test.failure.ignore=true clean package"
-            }
+            
         stage('Test') {
             steps {
                junit '**/target/surefire-reports/TEST-*.xml' 
             }
         }
 
-        stage('Deploy') {
+        stage('Archive') {
             steps {
                 archiveArtifacts 'target/*.jar'
             }
         }
 
-            post {
-                // If Maven was able to run the tests, even if some of the test
-                // failed, record the test results and archive the jar file.
-               success {
-                    junit '**/target/surefire-reports/TEST-*.xml'
-                    archiveArtifacts 'target/*.jar'
-                }
+        stage('Deploy'){
+            steps{
+                echo "deploying application...."
             }
         }
+
+            //post {
+                // If Maven was able to run the tests, even if some of the test
+                // failed, record the test results and archive the jar file.
+               //success {
+                    //junit '**/target/surefire-reports/TEST-*.xml'
+                    //archiveArtifacts 'target/*.jar'
+               // }
+            //}
+        
     }
 }
