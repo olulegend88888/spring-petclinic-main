c804e9ab08143cfac80d28a0400979a967177351
cloudperis <cloudperis1.gmail.com>
2021-09-04 14:42:45 -0400

    done

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -45,9 +45,23 @@ pipeline {
             }
         }
 
-        stage('Deploy'){
+        stage('Publish-Artifact'){
             steps{
                 s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false, dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'pet-clinic-shay', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'us-east-1', showDirectlyInBrowser: false, sourceFile: 'target/*.jar', storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: 'Jenkins-user', userMetadata: []
+            }
+        }
+
+        stage('Deploy'){
+            steps{
+                echo 'deploying application updates....'
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding',
+                accessKeyVariable: 'AWS_ACCESS_KEY_ID',
+                credentialsId: 'aws-cred',
+                secretKeyVariable: 'AWS_SECRET_ACCESS_KEY'
+                ]])
+                {
+                  sh 'aws ec2 reboot-instances --instance-ids ${params.devserver}'
+                }
             }
         }
 
