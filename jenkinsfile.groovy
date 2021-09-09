e8cd3b4b49308856670e0f768839ef09435419d3
cloudperis <cloudperis1.gmail.com>
2021-09-09 17:13:57 -0400

    DONE

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -58,4 +58,9 @@ pipeline {
 
         
     }
+    post{
+        success{
+           slackSend channel: 'jenkins-notifications', message: 'SUCCESS ${env.JOB_NAME} at ${BUILD_TIMESTAMP}' 
+        }
+    }
 }
