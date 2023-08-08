aae7b8d88ad99e36b2a1373943c94711506bd266
olulegend88888 <bodeige81@gmail.com>
2023-08-08 14:30:13 -0500

    s3 update

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -35,7 +35,7 @@ pipeline {
 
         stage('Publish-Artifact'){
             steps{
-                s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false, dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'olulegends3', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'us-east-2', showDirectlyInBrowser: false, sourceFile: 'target/*.jar', storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: 'jenkinss3', userMetadata: []
+                s3Upload acl: 'Private', bucket: 'olulegends3', cacheControl: '', excludePathPattern: '', file: 'target/*.jar', includePathPattern: '', metadatas: [''], redirectLocation: '', sseAlgorithm: '', tags: '', text: '', workingDir: ''
             }
         }
 
