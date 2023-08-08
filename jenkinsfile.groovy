9e0afbe80227c2526372fe6fe8c84f3ed3bc165d
olulegend88888 <bodeige81@gmail.com>
2023-08-07 21:49:46 -0500

    bodex

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -12,7 +12,7 @@ pipeline {
         
         stage('Build') {
             steps{
-                
+                slackSend channel: 'bode-alert', color: '#2211d9', message: "STARTED ${env.JOB_NAME} at #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
                 sh "mvn package"
 
             }
@@ -34,21 +34,21 @@ pipeline {
 
         stage('Publish-Artifact'){
             steps{
-                s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false, dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'app-seun-pet-clinic', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'us-east-1', showDirectlyInBrowser: false, sourceFile: 'target/*.jar', storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: 'Jenkins', userMetadata: []
+                s3Upload consoleLogLevel: 'INFO', dontSetBuildResultOnFailure: false, dontWaitForConcurrentBuildCompletion: false, entries: [[bucket: 'olulegends3', excludedFile: '', flatten: false, gzipFiles: false, keepForever: false, managedArtifacts: false, noUploadOnFailure: true, selectedRegion: 'us-east-2', showDirectlyInBrowser: false, sourceFile: 'target/*.jar', storageClass: 'STANDARD', uploadFromSlave: false, useServerSideEncryption: false]], pluginFailureResultConstraint: 'FAILURE', profileName: 'jenkinss3', userMetadata: []
             }
         }
 
-       stage('Deploy-Dev'){
+       stage('Deploy-Dev1'){
             steps{
                 echo 'deploying application updates....'
                 withCredentials([[
                       $class: 'AmazonWebServicesCredentialsBinding',
-                      credentialsId: "Jenkins-aws",
+                      credentialsId: "jenkinss3",
                       accessKeyVariable: 'AWS_ACCESS_KEY_ID',
-                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]) {
+                      secretKeyVariable: 'AWS_SECRET_ACCESS_KEY']]){ 
 
-                          
-                          sh "aws ec2 reboot-instances --instance-ids ${params.devserver} --region us-east-1"
+
+                          sh "aws ec2 reboot-instances --instance-ids i-0e1862772a5afb538 --region us-east-2"
 
                       }
 
@@ -60,10 +60,10 @@ pipeline {
     }
     post{
         success{
-           slackSend channel: 'jenkins-notifications', color: '439FE0', message: "SUCCESS ${currentBuild.fullDisplayName} at ${currentBuild.durationString[0..-13]}" 
+           slackSend channel: 'bode-alert', color: '439FE0', message: "SUCCESS ${currentBuild.fullDisplayName} at ${currentBuild.durationString[0..-13]}" 
         }
         failure{
-            slackSend channel: 'jenkins-notifications', color: '#fc0303', message: "FAILURE ${currentBuild.fullDisplayName} at ${currentBuild.durationString[0..-13]}"
+            slackSend channel: 'bode-alert', color: '#fc0303', message: "FAILURE ${currentBuild.fullDisplayName} at ${currentBuild.durationString[0..-13]}"
         }
     }
 }
